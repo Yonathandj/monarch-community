06ca692ada88cf39a569af967cb046600f7d6426
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-20 21:11:38 +0700

    Initialize monarch community project

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+
+import "./globals.css";
+
+export const metadata = {
+  title: {
+    template: "%s | Monarch Community",
+    default: "Monarch Community",
+  },
+  description: "Share your ideas with people around the world through blogs",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
