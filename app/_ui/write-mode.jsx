0a0791497b8c0b3b72fd4dbf2a77de17178d209b
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-26 20:16:22 +0700

    Add shadcn textarea & edit single image dropzone

---
+++ app/_ui/write-mode.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useContext } from "react";
+import { PostContext } from "../_provider/post-context-provider";
+
+import { Textarea } from "@/components/ui/textarea";
+
+import { SingleImageDropzone } from "./single-image-dropzone";
+
+export default function WriteMode() {
+  const { post, setPost } = useContext(PostContext);
+
+  return (
+    <form>
+      <SingleImageDropzone
+        width={450}
+        height={250}
+        value={post.headerImage}
+        onChange={(file) => {
+          setPost({ ...post, headerImage: file });
+        }}
+        className="mx-auto"
+      />
+      <Textarea
+        name="title"
+        placeholder="Untitled post"
+        className="mt-4 border-none shadow-none text-4xl focus-visible:ring-0 font-bold resize-none overflow-hidden"
+      />
+    </form>
+  );
+}
--- app/posts/write/page.jsx
+++ app/posts/write/page.jsx
@@ -1,7 +1,7 @@
+import WriteMode from "@/app/_ui/write-mode";
+
 export default function Page({ searchParams: { preview } }) {
   return (
-    <main className="mt-6">
-      <h2>Hehe</h2>
-    </main>
+    <main className="mt-6">{preview ? <h2>Preview</h2> : <WriteMode />}</main>
   );
 }
