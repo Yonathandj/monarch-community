a3d7391509ac2ad1cacf3558fae2a8e9ce43603c
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-27 19:28:55 +0700

    Fix code

--- app/_ui/write-preview-switcher.jsx
+++ app/_ui/write-preview-switcher.jsx
@@ -4,7 +4,8 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import { ActivityLogIcon } from "@radix-ui/react-icons";
+
+import { FileTextIcon } from "@radix-ui/react-icons";
 
 export default function WritePreviewSwitcher() {
   const pathname = usePathname();
@@ -25,7 +26,7 @@ export default function WritePreviewSwitcher() {
   return (
     <section className="flex gap-x-2 items-center">
       <Label htmlFor="preview-switcher">
-        <ActivityLogIcon className="w-6 h-6" />
+        <FileTextIcon className="w-6 h-6" />
       </Label>
       <Switch
         id="preview-switcher"
