85b7fd74e982e8e0b925c2ff82e21abc6725a70b
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-28 12:09:10 +0700

    Fix code sign in and sign up

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,9 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
+export default function Page({ searchParams: { redirect_url } }) {
   return (
     <main className="flex justify-center items-center mt-10">
-      <SignIn />
+      <SignIn afterSignInUrl={redirect_url || "/"} />
     </main>
   );
 }
--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -1,9 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
+export default function Page({ searchParams: { redirect_url } }) {
   return (
     <main className="flex justify-center items-center mt-5">
-      <SignUp />
+      <SignUp afterSignUpUrl={redirect_url || "/"} />
     </main>
   );
 }
