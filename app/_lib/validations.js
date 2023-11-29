07070acfab4243fb2b2aae00db07041257be4f46
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-29 09:23:53 +0700

    Fix code in route handler post write

--- app/_lib/validations.js
+++ app/_lib/validations.js
@@ -4,12 +4,13 @@ export const userValidationSchema = z.object({
     userId: z.string().trim(),
     fullName: z.string().trim(),
     email: z.string().trim().email(),
-    profileImageURL: z.string().trim().url().optional(),
+    profileImageURL: z.string().trim().url(),
 })
 
 export const postValidationSchema = z.object({
     userId: z.string().trim(),
     content: z.string().trim(),
+    headerImageURL: z.any().optional(),
     title: z.string().trim().optional(),
     tags: z.array(z.string()).optional(),
 })
--- app/api/posts/route.js
+++ app/api/posts/route.js
@@ -5,29 +5,31 @@ import { addUnpublishedPost, updateUnpublishedPost } from "@/app/_lib/services";
 import { postValidationSchema } from "@/app/_lib/validations";
 
 export async function POST(request) {
-    const { userId, unpublishedPost } = await request.json();
+    try {
+        const { userId, unpublishedPost } = await request.json();
 
-    const validationResult = postValidationSchema.safeParse({ userId, ...unpublishedPost });
-    if (!validationResult.success) {
-        return new Response('Bad request! Please provide valid data', { status: 400 });
-    }
-    if (validationResult.success) {
-        const { userId, title, tags, content } = validationResult.data;
-
-        const unpublishedPostInDatabase = await getUnpublishedPost(userId);
-        if (unpublishedPostInDatabase) {
-            const response = await updateUnpublishedPost({ userId, title, tags, content, headerImageURL: unpublishedPost.headerImageURL });
-            if (response) {
-                return new Response('unpublished post successfully updated', { status: 200 });
-            }
-            return new Response('Something went wrong with the system! Try again!', { status: 500 })
-        } else {
-            const response = await addUnpublishedPost({ userId, title, tags, content, headerImageURL: unpublishedPost.headerImageURL })
-            if (response) {
-                return new Response('New unpublished post successfully added', { status: 200 });
-            }
-            return new Response('Something went wrong with the system! Try again!', { status: 500 })
+        const validationResult = postValidationSchema.safeParse({ userId, ...unpublishedPost });
+        if (!validationResult.success) {
+            return new Response('Bad request! Please provide valid data', { status: 400 });
         }
+        if (validationResult.success) {
+            const { userId, title, tags, content, headerImageURL } = validationResult.data;
+
+            const unpublishedPostInDatabase = await getUnpublishedPost(userId);
+
+            if (unpublishedPostInDatabase) {
+                const response = await updateUnpublishedPost({ userId, title, tags, content, headerImageURL });
+                if (response) {
+                    return new Response('unpublished post successfully updated', { status: 200 });
+                }
+            } else {
+                const response = await addUnpublishedPost({ userId, title, tags, content, headerImageURL })
+                if (response) {
+                    return new Response('New unpublished post successfully added', { status: 200 });
+                }
+            }
+        }
+    } catch (error) {
+        return new Response('Something went wrong with the system! Try again!', { status: 500 })
     }
-    return new Response('Bad request!', { status: 400 })
 }
