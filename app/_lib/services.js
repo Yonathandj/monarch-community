80dc38e8cd61785b49780bfca0cbfe55e7897a4a
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-29 09:44:06 +0700

    Update code route handler add new user

--- app/_lib/services.js
+++ app/_lib/services.js
@@ -1,8 +1,23 @@
 import { nanoid } from "nanoid";
 
 import post from "./_models/post";
+import user from "./_models/user";
 
 import connectDB from "./connectDB"
+
+export const addNewUser = async ({ userId, fullName, email, profileImageURL }) => {
+    try {
+        connectDB();
+        const _id = `monarchUserId-${nanoid(16)}`;
+        const newUser = new user({
+            _id, clerk: { userId, fullName, email, profileImageURL }
+        })
+        const response = await newUser.save();
+        return response;
+    } catch (error) {
+        throw new Error(`Failed to update unpublished post! ${error}`)
+    }
+}
 
 export const updateUnpublishedPost = async ({ userId, title, tags, content, headerImageURL }) => {
     try {
--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,76 +1,82 @@
 import { Webhook } from 'svix'
-import { nanoid } from 'nanoid';
 import { headers } from 'next/headers'
 
-import user from '@/app/_lib/_models/user';
-import connectDB from '@/app/_lib/connectDB';
+import { getUserById } from '@/app/_lib/data';
+import { addNewUser } from '@/app/_lib/services';
+import { userValidationSchema } from '@/app/_lib/validations';
 
 export async function POST(req) {
-    const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-    if (!WEBHOOK_SECRET) {
-        return new Response('Error occured', {
-            status: 500
-        })
-    }
-
-    const headerPayload = headers();
-    const svix_id = headerPayload.get("svix-id");
-    const svix_timestamp = headerPayload.get("svix-timestamp");
-    const svix_signature = headerPayload.get("svix-signature");
-
-    if (!svix_id || !svix_timestamp || !svix_signature) {
-        return new Response('Error occured - no svix headers', {
-            status: 400
-        })
-    }
-
-    let event
-
-    const bodyPayload = await req.json()
-    const bodyPayloadStringified = JSON.stringify(bodyPayload);
-
-    const webhook = new Webhook(WEBHOOK_SECRET);
-
     try {
-        event = webhook.verify(bodyPayloadStringified, {
-            "svix-id": svix_id,
-            "svix-timestamp": svix_timestamp,
-            "svix-signature": svix_signature,
-        })
-    } catch (err) {
-        console.error('Error verifying webhook:', err);
-        return new Response('Error occured', {
-            status: 400
-        })
-    }
+        const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+        if (!WEBHOOK_SECRET) {
+            return new Response('Error occured', {
+                status: 500
+            })
+        }
 
-    if (event.type === 'user.created') {
-        connectDB()
+        const headerPayload = headers();
+        const svix_id = headerPayload.get("svix-id");
+        const svix_timestamp = headerPayload.get("svix-timestamp");
+        const svix_signature = headerPayload.get("svix-signature");
 
-        const { id: userId } = event.data;
-        const userAlreadyExist = await user.findOne({ userId }).exec();
-
-        if (userAlreadyExist) {
-            return new Response('User already exist', {
+        if (!svix_id || !svix_timestamp || !svix_signature) {
+            return new Response('Error occured - no svix headers', {
                 status: 400
-            });
+            })
         }
-        const _id = `monarchUser_${nanoid(16)}`;
-        const clerk = {
-            userId: event.data.id,
-            fullName: event.data?.first_name || event.data?.last_name ? `${event.data?.first_name || ''} ${event.data?.last_name || ''}` : event.data.email_addresses[0].email_address.split('@')[0],
-            email: event.data.email_addresses[0].email_address,
-            profileImageURL: event.data.image_url,
+
+        let event
+
+        const bodyPayload = await req.json()
+        const bodyPayloadStringified = JSON.stringify(bodyPayload);
+
+        const webhook = new Webhook(WEBHOOK_SECRET);
+
+        try {
+            event = webhook.verify(bodyPayloadStringified, {
+                "svix-id": svix_id,
+                "svix-timestamp": svix_timestamp,
+                "svix-signature": svix_signature,
+            })
+        } catch (err) {
+            console.error('Error verifying webhook:', err);
+            return new Response('Error occured', {
+                status: 400
+            })
         }
-        const newUser = new user({
-            _id,
-            clerk
+
+        if (event.type === 'user.created') {
+            const validationResult = userValidationSchema.safeParse({
+                userId: event.data.id,
+                fullName: event.data?.first_name || event.data?.last_name ? `${event.data?.first_name || ''} ${event.data?.last_name || ''}` : event.data.email_addresses[0].email_address.split('@')[0],
+                email: event.data.email_addresses[0].email_address,
+                profileImageURL: event.data.image_url,
+            })
+            if (!validationResult.success) {
+                return new Response('Bad request! Please provide valid data', {
+                    status: 400
+                });
+            }
+            if (validation.success) {
+                const { userId, fullName, email, profileImageURL } = validationResult.data;
+                const userAlreadyExist = await getUserById(userId);
+                if (userAlreadyExist) {
+                    return new Response('User already exist', {
+                        status: 400
+                    });
+                } else {
+                    const response = await addNewUser({ userId, fullName, email, profileImageURL });
+                    if (response) {
+                        return new Response('New user successfully created', {
+                            status: 200
+                        });
+                    }
+                }
+            }
+        }
+    } catch (error) {
+        return new Response('Something went wrong with the system! Try again', {
+            status: 500
         });
-        const result = await newUser.save();
-        if (result) {
-            return new Response('User created successfully', { status: 200 })
-        }
-        return new Response('Something went wrong with the system! Try again!', { status: 500 })
     }
-    return new Response('Not found', { status: 404 })
 }
