d17af05f356cdb0b92b0bb3f7980d82f904e4d68
Yonathan Dawang Jati <yonathandawang111@gmail.com>
2023-11-29 11:44:55 +0700

    Handle bug code

--- app/_ui/signed-in-card.jsx
+++ app/_ui/signed-in-card.jsx
@@ -15,16 +15,17 @@ import SignOutButton from "./sign-out-button";
 
 export default async function SignedInCard() {
   const { userId } = auth();
-  const {
-    clerk: { fullName },
-  } = await getUserById(userId);
+  const selectedUser = await getUserById(userId);
 
   return (
     <Card>
       <CardHeader>
         <CardTitle className="text-lg font-bold">
-          Welcome <span className="text-purple-600">{fullName}</span> to Monarch
-          Community
+          Welcome{" "}
+          <span className="text-purple-600">
+            {selectedUser?.clerk?.fullName}
+          </span>{" "}
+          to Monarch Community
         </CardTitle>
         <CardDescription>
           Share your thoughts and ideas now for everyone around the world
